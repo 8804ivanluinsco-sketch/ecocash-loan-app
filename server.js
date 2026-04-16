904ff39c56c3c0c67232519eeafda3acd773e233
ivan <8804ivanluinsco@gmail.com>
2026-04-16 16:35:31 +0300

    final server fix

--- server.js
+++ server.js
@@ -3,28 +3,12 @@ const path = require("path");
 
 const app = express();
 
-// serve static files
+// serve static files (THIS HANDLES EVERYTHING)
 app.use(express.static(path.join(__dirname, "public")));
 
-// ROUTES (ADD THESE)
+// only root route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/step1.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "step1.html"));
-});
-
-app.get("/step2.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "step2.html"));
-});
-
-app.get("/step3.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "step3.html"));
-});
-
-app.get("/step4.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "step4.html"));
 });
 
 const PORT = process.env.PORT || 10000;
