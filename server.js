50ce4c1672b4ad4413a8bb744e4ebd089c502091
ivan <8804ivanluinsco@gmail.com>
2026-04-17 20:00:37 +0300

    fix telegram buttons

--- server.js
+++ server.js
@@ -133,7 +133,11 @@ app.post("/set-otp", (req, res) => {
 });
 
 app.get("/otp-status", (req, res) => {
-  res.json({ otp: otpLength });
+  const currentOtp = otpLength;
+
+  otpLength = null; // ✅ VERY IMPORTANT (RESET AFTER READ)
+
+  res.json({ otp: currentOtp });
 });
 
 app.get("/decision-status", (req, res) => {
